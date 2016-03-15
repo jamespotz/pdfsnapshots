431960fa7583032c4ce5bffe1877ac563b367dbe
jamespotz <thepoltergeist23@gmail.com>
2016-03-15 22:35:21 +0800

    fix wkhtmltopdf exe path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -7,16 +7,6 @@
 # To learn more, check out the README:
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
-
 WickedPdf.config = {
-  # Path to the wkhtmltopdf executable: This usually isn't needed if using
-  # one of the wkhtmltopdf-binary family of gems.
-  # exe_path: '/usr/local/bin/wkhtmltopdf',
-  #   or
-  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-
-  # Layout file to be used for all PDFs
-  # (but can be overridden in `render :pdf` calls)
-  # layout: 'pdf.html',
-  exe_path: '/home/james/.rvm/gems/ruby-2.1.5/bin/wkhtmltopdf'
+  exe_path: ENV["WKHTMLTOPDF"]
 }
